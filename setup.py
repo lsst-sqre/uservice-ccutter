74fd6a238512c1a8471dd5acfbecd036fcb1dabb
Jonathan Sick <jsick@lsst.org>
2017-10-19 21:31:15 -0700

    0.1.0 release
    - Update setup.py
    - Update image versions in k8s deployment

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ DESCRIPTION = 'Bootstrapper for cookiecutter projects'
 AUTHOR = 'Adam Thornton'
 AUTHOR_EMAIL = 'athornton@lsst.org'
 URL = 'https://github.com/sqre-lsst/uservice-ccutter'
-VERSION = '0.0.9'
+VERSION = '0.1.0'
 LICENSE = 'MIT'
 
 
